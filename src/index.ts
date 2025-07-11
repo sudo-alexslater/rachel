2f40a911708b98bfca927152724fdfcbe9a51e92
Alex Slater <dev@alexslater.io>
2025-07-11 21:12:23 +0100

    raem

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+function sayHello(name: string) {
+  let message: string = "hello ";
+  // before
+  message += name;
+  // after
+  console.log(message);
+}
+
+sayHello("stinky");
+sayHello("stinky");
+sayHello("big man");
+
+//this is a comment
